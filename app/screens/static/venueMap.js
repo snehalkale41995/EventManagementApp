bfa658d691e8aee10cbb19089958f92952c08ed4
Snehal Kale <snehal.kale@eternussolutions.com>
2018-08-29 20:56:23 +0530

    added dynamic venueMap screen

--- app/screens/static/venueMap.js
+++ app/screens/static/venueMap.js
@@ -6,7 +6,8 @@ import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
 import getDirections from 'react-native-google-maps-directions';
 import { Button } from 'react-native';
-
+import * as infoService from '../../serviceActions/staticPages';
+import * as eventService from '../../serviceActions/event';
 
 function renderIf(condition, content) {
   if (condition) {
@@ -25,22 +26,19 @@ export class VenueMap extends React.Component {
     super(props);
     this.state = {
       isOffline: false,
-      initialRegion: {
-        latitude: 18.5392208,
-        longitude: 73.9063,
-        latitudeDelta:  0.0922,
-        longitudeDelta: 0.0421
-      }
+      locationInfo : {},
+      eventInfo : {}
     }
   }
 
   handleGetDirections = () => {
+    let locationData = this.state.locationInfo;
     const data = {
       destination: {
-        latitude: 18.5392208,
-        longitude: 73.9063,
-        latitudeDelta:  0.0922,
-        longitudeDelta: 0.0421
+        latitude: locationData.latitude,
+        longitude: locationData.longitude,
+        latitudeDelta:locationData.latitudeDelta,
+        longitudeDelta: locationData.longitudeDelta
       },
       params: [
         {
@@ -53,13 +51,13 @@ export class VenueMap extends React.Component {
         }
       ]
     }
- 
     getDirections(data)
   }
   componentWillMount() {
     if (Platform.OS !== 'ios') {
       NetInfo.isConnected.fetch().then(isConnected => {
         if (isConnected) {
+          this.getLocationInfo();
           this.setState({
             isLoading: true
           });
@@ -74,6 +72,7 @@ export class VenueMap extends React.Component {
         });
       });
     }
+     this.getLocationInfo();
     NetInfo.addEventListener(
       'connectionChange',
       this.handleFirstConnectivityChange
@@ -82,6 +81,7 @@ export class VenueMap extends React.Component {
 
   handleFirstConnectivityChange = (connectionInfo) => {
     if (connectionInfo.type != 'none') {
+       this.getLocationInfo();
       this.setState({
         isLoading: true
       });
@@ -102,45 +102,118 @@ export class VenueMap extends React.Component {
       this.handleFirstConnectivityChange
     );
   }
-  render() {
+
+    getLocationInfo(){
+    eventService.getCurrentEvent((eventInfo)=>{
+      if(eventInfo){
+        let eventId = eventInfo._id;
+       infoService.getLocationInfo(eventId).then((response)=>{
+        let data = response[0];
+        let eventInfo = {
+          eventName : data.event.eventName,
+          address : data.address
+       }
+       let locationInfo = {
+        latitude: data.latitude,
+        longitude: data.longitude,
+        latitudeDelta: data.latitudeDelta,
+        longitudeDelta: data.longitudeDelta
+       }
+        this.setState(
+          {
+           eventInfo:eventInfo,
+           locationInfo:locationInfo, 
+           isLoaded: true}
+        )
+      }).catch((error)=>{
+       // console.log(error);
+       })
+      }
+      else{
+        return;
+      }
+    })
+  }
+  
+displayMap(){
+    let locationInfo = this.state.locationInfo;
+    let eventInfo = this.state.eventInfo;
+    let eventLatitude = locationInfo.latitude;
+    let eventLongitude = locationInfo.longitude;
     return (
       <Container style={[styles.root]}>
-        
         <RkCard rkType='shadowed' style={[styles.card]}>
-           
-            <Text style={{ fontSize: 16, fontWeight: 'bold',marginBottom:6 }}>TiECon Pune 2018</Text> 
-            <Text style={{ fontSize: 15, }}>The Westin Pune Koregaon Park, 36/3-B, Mundhwa Rd, Pingale Wasti, Koregaon Park Annexe, Ghorpadi, Pune, Maharashtra 411001</Text>
-           
+            <Text style={{ fontSize: 16, fontWeight: 'bold',marginBottom:6 }}>{eventInfo.eventName}</Text> 
+            <Text style={{ fontSize: 15, }}>{eventInfo.address}</Text>
         </RkCard>
         <Button onPress={this.handleGetDirections} title="Get Directions">
         </Button>
-        
         <MapView style={styles1.map}
-          initialRegion={this.state.initialRegion}>
+          initialRegion={locationInfo}>
           <Marker
-            coordinate={{latitude:18.5392208,longitude:73.90626109999994}}
-            title={'TiECon Pune 2018'}
-            description={'The Westin Pune Koregaon Park, 36/3-B, Mundhwa Rd, Pingale Wasti, Koregaon Park Annexe, Ghorpadi, Pune, Maharashtra 411001'}
+            coordinate={{latitude:eventLatitude,longitude:eventLongitude}}
+            title={eventInfo.eventName}
+            description={eventInfo.address}
           />
         </MapView>
-        
         <View style={styles.footerOffline}>
           {
             this.state.isOffline ? <RkText rkType="small" style={styles.footerText}>The Internet connection appears to be offline. </RkText> : null
           }
         </View>
-        
         <View style={styles.footer} style={{ flexDirection: 'row',justifyContent: 'center',  position:'absolute', backgroundColor:'red',bottom:0, alignSelf: 'stretch', width: '100%'}}>
           <RkText rkType="small" style={styles.footerText}>Powered by</RkText>
           <RkText rkType="small" style={styles.companyName}> Eternus Solutions Pvt. Ltd. </RkText>
         </View>
-      
       </Container>
-     
-      
     )
-  }
 }
+
+  render() {
+  let map = this.displayMap();
+        if (this.state.isLoaded) {
+            return (
+                <Container style={[styles.root]}>
+                    <ScrollView>
+                        <View>
+                           {map}
+                        </View>
+                    </ScrollView>
+                    <View style={[styles.footerOffline]}>
+                        {
+                            this.state.isOffline ? <RkText rkType="small" style={[styles.footerText]}>The Internet connection appears to be offline. </RkText> : null
+                        }
+                    </View>
+                    <View style={[styles.footer]}>
+                        <RkText rkType="small" style={[styles.footerText]}>Powered by</RkText>
+                        <RkText rkType="small" style={[styles.companyName]}> Eternus Solutions Pvt. Ltd. </RkText>
+                    </View>
+                </Container>
+            )
+        }
+        else {
+            return (
+                <Container style={[styles.root]}>
+                    <ScrollView>
+                    <View style={[styles.loading]}>
+                        <ActivityIndicator size='small' />
+                    </View>
+                    </ScrollView>
+                    <View style={[styles.footerOffline]}>
+                        {
+                            this.state.isOffline ? <RkText rkType="small" style={[styles.footerText]}>The Internet connection appears to be offline. </RkText> : null
+                        }
+                    </View>
+                    <View style={[styles.footer]}>
+                        <RkText rkType="small" style={[styles.footerText]}>Powered by</RkText>
+                        <RkText rkType="small" style={[styles.companyName]}> Eternus Solutions Pvt. Ltd. </RkText>
+                    </View>
+                </Container>
+            )
+        }
+     }
+}
+
 const styles1 = StyleSheet.create({
   container: {
     position: 'absolute',
