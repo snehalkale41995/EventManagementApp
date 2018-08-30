be6195bd7cb366a37fe43634eabd84aa948fce49
Snehal Kale <snehal.kale@eternussolutions.com>
2018-08-30 20:43:58 +0530

    added sponsor screen

--- app/screens/sponsors/sponsors.js
+++ app/screens/sponsors/sponsors.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { ScrollView, Platform, Image } from 'react-native';
+import { ScrollView, Platform, Image, NetInfo} from 'react-native';
 import { Text, View, Icon, Container, Label } from 'native-base';
 import { StyleSheet, FlatList, TouchableOpacity, Keyboard, Alert, AsyncStorage, ActivityIndicator, Linking } from 'react-native';
 import { RkComponent, RkTheme, RkStyleSheet, RkText, RkAvoidKeyboard, RkButton, RkCard, RkChoice, RkTextInput, RkChoiceGroup } from 'react-native-ui-kitten';
 import { NavigationActions } from 'react-navigation';
 import ReactMoment from 'react-moment';
 import { GradientButton } from '../../components/gradientButton';
-import { Service } from '../../services';
-import Moment from 'moment';
-import firebase from './../../config/firebase';
 import { Avatar } from '../../components';
-var firestoreDB = firebase.firestore();
+import * as sponsorService from '../../serviceActions/staticPages';
+import * as eventService from '../../serviceActions/event';
 
 export class Sponsors extends RkComponent {
     static navigationOptions = {
@@ -18,52 +16,100 @@ export class Sponsors extends RkComponent {
     };
     constructor(props) {
         super(props);
-
         this.state = {
-            Sponsers: [],
-            isLoaded: false
+            Sponsors: [],
+            isLoaded: false,
+            isOffline: false
         }
     }
-    componentWillMount() {
-        let thisRef = this;
-        let sponserCollection = [];
-        firestoreDB.collection("Sponsor").orderBy("orderNumber", "asc")
-            .get()
-            .then(function (doc) {
-                doc.forEach(fItem => {
-                    let itemData = fItem.data();
-                    let itemId = fItem.id;
-                    sponserCollection.push({ sponserData: itemData, sponserId: itemId });
-                })
-                thisRef.setState({
-                    Sponsers: sponserCollection,
-                    isLoaded: true
-                })
-                thisRef.displaySponsers();
-            })
-            .catch(function (error) {
-                // console.log("error",error);
+
+     componentWillMount() {
+        if (Platform.OS !== 'ios') {
+            NetInfo.isConnected.fetch().then(isConnected => {
+                if (isConnected) {
+                    this.getSponsorsList();
+                } else {
+                    this.setState({
+                        isOffline: true
+                    });
+                }
+                this.setState({
+                    isOffline: !isConnected
+                });
             });
+        }
+        this.getSponsorsList();
+        NetInfo.addEventListener(
+            'connectionChange',
+            this.handleFirstConnectivityChange
+        );
     }
 
-    displaySponsers = () => {
-        return this.state.Sponsers.map((sponser, index) => {
+    handleFirstConnectivityChange = (connectionInfo) => {
+        if (connectionInfo.type != 'none') {
+            this.getSponsorsList();
+        } else {
+            this.setState({
+                isOffline: true
+            });
+        }
+        this.setState({
+            isOffline: connectionInfo.type === 'none',
+        });
+    };
+
+    componentWillUnmount() {
+        NetInfo.removeEventListener(
+            'connectionChange',
+            this.handleFirstConnectivityChange
+        );
+    }
+
+    getSponsorsList() {
+        let thisRef = this;
+        let sponserCollection = [];
+        eventService.getCurrentEvent((eventInfo)=>{
+        if(eventInfo){
+        let eventId = eventInfo._id;
+        sponsorService.getSponsorInfo(eventId).then((response)=>{
+        this.setState(
+          {
+            Sponsors: response,
+            isLoaded: true
+          }
+        )
+      }).catch((error)=>{
+        this.setState({ isLoaded: false})
+       })
+       }
+    })
+    }
+  
+     displayWebsite(websiteURL){
+     if(websiteURL){
+      Linking.openURL(websiteURL); 
+     }
+      else return;
+     }
+
+    displaySponsors = () => {
+        return this.state.Sponsors.map((sponsor, index) => {
             let avatar;
-            if (sponser.sponserData.imageURL) {
-                avatar = <Image style={{ width: 60, height: 60 }} source={{ uri: sponser.sponserData.imageURL }} />
+            if (sponsor.imageURL) {
+                avatar = <Image style={{ width: 60, height: 60 }} source={{ uri: sponsor.imageURL }} />
             } else {
                 avatar = <Image style={{ width: 60, height: 60 }} source={require('../../assets/images/defaultSponsorImg.png')} />
             }
             return (
-                <TouchableOpacity onPress={() => Linking.openURL(sponser.sponserData.websiteURL)}>
+                <TouchableOpacity onPress={() => this.displayWebsite(sponsor.websiteURL)}>
                     <RkCard rkType='shadowed' style={[styles.card]}>
                         <View style={{ flexDirection: 'row' }}>
                             <View style={{ flexDirection: 'column', alignItems: 'flex-start', marginVertical: 10, flex: 3, alignSelf: 'center', marginLeft: 10 }}>
                                 {avatar}
                             </View>
                             <View style={{ flexDirection: 'column', alignItems: 'flex-start', marginVertical: 10, flex: 6, marginLeft: -10 }}>
-                                <Text style={styles.headerText}>{sponser.sponserData.category}</Text>
-                                <Text style={styles.infoText}>{sponser.sponserData.name}</Text>
+                                <Text style={styles.headerText}>{sponsor.category}</Text>
+                                <Text style={styles.infoText}>{sponsor.name}</Text>
                             </View >
                         </View >
                     </RkCard>
@@ -71,42 +117,55 @@ export class Sponsors extends RkComponent {
             )
         });
     }
-    render() {
-        let sponserList = this.displaySponsers();
+
+        render() {
+        let sponsorList = this.displaySponsors();
         if (this.state.isLoaded) {
             return (
                 <Container style={[styles.root]}>
                     <ScrollView>
                         <View>
-                            {sponserList}
+                             {sponsorList} 
                         </View>
                     </ScrollView>
+                    <View style={[styles.footerOffline]}>
+                        {
+                            this.state.isOffline ? <RkText rkType="small" style={[styles.footerText]}>The Internet connection appears to be offline. </RkText> : null
+                        }
+                    </View>
+                    <View style={[styles.footer]}>
+                        <RkText rkType="small" style={[styles.footerText]}>Powered by</RkText>
+                        <RkText rkType="small" style={[styles.companyName]}> Eternus Solutions Pvt. Ltd. </RkText>
+                    </View>
                 </Container>
             )
         }
         else {
             return (
                 <Container style={[styles.root]}>
+                    <ScrollView>
                     <View style={[styles.loading]}>
                         <ActivityIndicator size='small' />
+                    </View>
+                    </ScrollView>
+                    <View style={[styles.footerOffline]}>
+                        {
+                            this.state.isOffline ? <RkText rkType="small" style={[styles.footerText]}>The Internet connection appears to be offline. </RkText> : null
+                        }
+                    </View>
+                    <View style={[styles.footer]}>
+                        <RkText rkType="small" style={[styles.footerText]}>Powered by</RkText>
+                        <RkText rkType="small" style={[styles.companyName]}> Eternus Solutions Pvt. Ltd. </RkText>
                     </View>
                 </Container>
             )
         }
     }
 }
+
 let styles = RkStyleSheet.create(theme => ({
     root: {
         backgroundColor: theme.colors.screen.base
-    },
-    attendBtn: {
-        flexDirection: 'column',
-        alignItems: 'center',
-        marginVertical: 10
-    },
-    listContainer: {
-        flex: 1,
-        flexDirection: 'column'
     },
     loading: {
         marginTop: 200,
@@ -122,18 +181,6 @@ let styles = RkStyleSheet.create(theme => ({
         margin: 1,
         padding: 4
     },
-    header: {
-        flexDirection: 'row'
-    },
-    mainHeader: {
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        marginLeft: 5
-    },
-    roomName: {
-        fontSize: 15,
-        marginLeft: 5,
-    },
     headerText: {
         fontWeight: 'bold',
         fontSize: 16
@@ -141,22 +188,27 @@ let styles = RkStyleSheet.create(theme => ({
     infoText: {
         fontSize: 12
     },
-    content: {
-        margin: 2,
-        padding: 2
-    },
-    duration: {
-        fontSize: 15,
-        marginLeft: 5,
-        marginRight: 10
-    },
-    tileIcons: {
-        paddingLeft: 4,
-        paddingTop: 4,
-        fontSize: 16
-    },
-    tileFooter: {
+      footer: {
         flexDirection: 'row',
-        alignContent: 'space-between'
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'stretch',
+        backgroundColor: '#E7060E'
+    },
+    footerOffline: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'stretch',
+        backgroundColor: '#545454'
+    },
+    footerText: {
+        color: '#f0f0f0',
+        fontSize: 11,
+    },
+    companyName: {
+        color: '#ffffff',
+        fontSize: 12,
+        fontWeight: 'bold'
     }
 }));
