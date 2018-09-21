9d790a2a0383f5f96da59af969431f0e70e98afe
Snehal Kale <snehal.kale@eternussolutions.com>
2018-09-21 12:24:31 +0530

    added emptydata component

---
+++ app/components/emptyData/emptyData.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { ActivityIndicator, ScrollView} from "react-native";
+import { View, Text, Container} from "native-base";
+import { RkStyleSheet } from "react-native-ui-kitten";
+import { Footer } from "../../components/footer";
+
+export class EmptyData extends React.Component {
+  render() {
+    return (
+      <Container style={[styles.root]}>
+        <ScrollView>
+          <View style={[styles.emptydata]}>
+            <Text>No Data Found</Text>
+          </View>
+        </ScrollView>
+        <View>
+          <Footer isOffline={this.props.isOffline} />
+        </View>
+      </Container>
+    );
+  }
+}
+
+let styles = RkStyleSheet.create(theme => ({
+  root: {
+    backgroundColor: theme.colors.screen.base
+  },
+  emptydata: {
+    marginTop: 200,
+    left: 0,
+    opacity: 0.5,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+}));
