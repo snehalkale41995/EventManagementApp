e0d70b25ebf96615261d2fec6d58438c8ce1fdfa
Snehal Kale <snehal.kale@eternussolutions.com>
2018-11-26 12:11:05 +0530

    removed socket reference

--- app/app.js
+++ app/app.js
@@ -12,8 +12,6 @@ import {data} from './data'
 import {AppLoading, Font} from 'expo';
 import {View, Text, Alert, AsyncStorage, Platform} from "react-native";
 import { Permissions, Notifications } from 'expo';
-import SocketIOClient from 'socket.io-client';
-const socket = SocketIOClient('http://192.168.101.21:3010');
 
 bootstrap();
 data.populateData();
@@ -64,20 +62,13 @@ export default class App extends React.Component {
     loaded: false,
     signedIn: false,
     checkedSignIn: false,
-    notification: {},
-     messages: []
+    notification: {}
   };
 
   componentWillMount() {
   this._loadAssets();
-  socket.emit('message', 'Hello world!');
-
-   socket.on('message', (message) => {
-   var oldMessages = this.state.messages;
-  // React will automatically rerender the component when a new message is added.
-   this.setState({ messages: oldMessages.concat(message) });
-   });
   }
+
   _loadAssets = async() => {
     await Font.loadAsync({
       'fontawesome': require('./assets/fonts/fontawesome.ttf'),
@@ -95,7 +86,6 @@ export default class App extends React.Component {
   };
 
   render() {
-     console.log("this.state.messages", this.state.messages)
     if (!this.state.loaded) {
       return <AppLoading />;
     }
