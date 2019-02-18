164b190b00ca74ceaf5762cf861c4c550ce7cf92
Snehal Kale <snehal.kale@eternussolutions.com>
2019-02-18 18:42:38 +0530

    added configurable fb,twitter url

--- app/screens/socialFeed/socialFeed.js
+++ app/screens/socialFeed/socialFeed.js
@@ -1,223 +1,227 @@
-import React from 'react';
-import { RkText, RkStyleSheet } from 'react-native-ui-kitten';
-import { Container } from 'native-base';
-import {  View, WebView, Text,NetInfo,Platform ,ScrollView,Cont } from 'react-native';
-import { scale, scaleModerate, scaleVertical } from '../../utils/scale';
-import * as loginService from '../../serviceActions/login';
-import QRCode from "react-native-qrcode"; 
-import {Loader} from '../../components/loader';
-import {Footer} from '../../components/footer'; 
-import { GradientButton } from '../../components/gradientButton';
-import * as eventService from '../../serviceActions/event';
-import {  Tab, TabHeading,Tabs } from "native-base";
-import { BackHandler } from 'react-native';
+import React from "react";
+import { RkText, RkStyleSheet } from "react-native-ui-kitten";
+import { Container } from "native-base";
+import {
+  View,
+  WebView,
+  Text,
+  NetInfo,
+  Platform,
+  ScrollView,
+  Cont
+} from "react-native";
+import { scale, scaleModerate, scaleVertical } from "../../utils/scale";
+import * as loginService from "../../serviceActions/login";
+import QRCode from "react-native-qrcode";
+import { Loader } from "../../components/loader";
+import { Footer } from "../../components/footer";
+import { GradientButton } from "../../components/gradientButton";
+import * as eventService from "../../serviceActions/event";
+import { Tab, TabHeading, Tabs } from "native-base";
+import { BackHandler } from "react-native";
+import * as infoService from "../../serviceActions/staticPages";
 
+
+let fbPageURL =
+  "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FTiEGlobal1%2F&tabs=timeline&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+let twitterPageURL =
+'<a class="twitter-timeline" href="https://twitter.com/TiEGlobal?ref_src=twsrc%5Etfw">Tweets by TiEGlobal</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
 export class SocialFeed extends React.Component {
   static navigationOptions = {
-    title: 'Social feed'.toUpperCase()
+    title: "Social feed".toUpperCase()
   };
 
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       isOffline: false,
-      eventInfo :{},
+      eventInfo: {},
       isLoaded: false,
-      fnameError:'',
-      lnameError:'',
-      contactError:'',
-      emailError:''
-    }
-
+      facebookUrl: "",
+      twitterUrl: ""
+    };
   }
 
-  handleBackPress=()=>{
-    //console.log('Here',this.props);
-      this.props.navigation.replace('HomeMenu');
-      return true;
+  handleBackPress = () => {
+    this.props.navigation.replace("HomeMenu");
+    return true;
+  };
 
-  }
- 
   componentWillMount() {
-    BackHandler.addEventListener('hardwareBackPress', this.handleBackPress);
+    BackHandler.addEventListener("hardwareBackPress", this.handleBackPress);
 
-    if (Platform.OS !== 'ios') {
+    if (Platform.OS !== "ios") {
       NetInfo.isConnected.fetch().then(isConnected => {
         if (isConnected) {
           this.getEventInfo();
         } else {
           this.setState({
             isOffline: true
-          }); 
+          });
         }
         this.setState({
           isOffline: !isConnected
         });
       });
     }
-     this.getEventInfo();
+    this.getEventInfo();
     NetInfo.addEventListener(
-      'connectionChange',
+      "connectionChange",
       this.handleFirstConnectivityChange
     );
   }
 
-
-  getEventInfo=()=>{
-    eventService.getCurrentEvent((eventDetails)=>{
-      if(eventDetails){
-        this.setState({
-          eventInfo : eventDetails,
-          isLoaded : true 
-        })
-       }
-      else{
-        this.setState({isLoaded:false})
+  getEventInfo() {
+    let eventInfo;
+    eventService.getCurrentEvent(eventInfo => {
+      if (eventInfo) {
+        let eventId = eventInfo._id;
+        infoService
+          .getEventInfo(eventId)
+          .then(response => {
+            if (response.length === 0) {
+              this.setState({
+                isLoaded: true,
+                facebookUrl: fbPageURL,
+                twitterUrl: twitterPageURL
+              });
+            } else {
+              eventInfo = response[0];
+              this.setState({
+                facebookUrl:
+                  eventInfo.facebookUrl !== ""
+                    ? eventInfo.facebookUrl
+                    : fbPageURL,
+                twitterUrl:
+                  eventInfo.twitterUrl !== ""
+                    ? eventInfo.twitterUrl
+                    : twitterPageURL,
+                isLoaded: true
+              });
+            }
+          })
+          .catch(error => {
+            this.setState({
+              isLoaded: true
+            });
+          });
+      } else {
+        return;
       }
-     })
+    });
   }
-  handleFirstConnectivityChange = (connectionInfo) => {
-    if (connectionInfo.type != 'none') {
+
+  handleFirstConnectivityChange = connectionInfo => {
+    if (connectionInfo.type != "none") {
       this.getEventInfo();
     } else {
       this.setState({
         isOffline: true
       });
-    } 
+    }
     this.setState({
-      isOffline: connectionInfo.type === 'none',
+      isOffline: connectionInfo.type === "none"
     });
   };
   componentWillUnmount() {
-    BackHandler.removeEventListener('hardwareBackPress',this.handleBackPress);
+    BackHandler.removeEventListener("hardwareBackPress", this.handleBackPress);
 
     NetInfo.removeEventListener(
-      'connectionChange',
+      "connectionChange",
       this.handleFirstConnectivityChange
     );
-  } 
+  }
   render() {
-    return( 
+    if (this.state.isLoaded) {
+      return (
+        <Container style={[styles.root]}>
+          <Tabs
+            tabBarUnderlineStyle={{ borderColor: "#00ffff" }}
+            style={{ elevation: 3, width: 400 }}
+            style={styles.tabContent}
+            locked={true}
+            onChangeTab={() => {}}
+          >
+            <Tab
+              heading="Twitter"
+              tabStyle={{ backgroundColor: "#f20505" }}
+              activeTabStyle={{ backgroundColor: "#f20505" }}
+              textStyle={{ fontSize: 15, color: "#fff" }}
+              activeTextStyle={{
+                fontSize: 15,
+                color: "#fff",
+                fontWeight: "bold"
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  alignContent: "center",
+                  justifyContent: "center",
+                  padding: 10,
+                  backgroundColor: "#d9d9d9"
+                }}
+              >
+                <WebView
+                  style={{ flex: 1 }}
+                  originWhitelist={["*"]}
+                  source={{
+                    html: this.state.twitterUrl
+                  }}
+                />
+              </View>
+            </Tab>
+            <Tab
+              heading="Facebook"
+              tabStyle={{ backgroundColor: "#f20505" }}
+              activeTabStyle={{ backgroundColor: "#f20505" }}
+              textStyle={{ fontSize: 15, color: "#fff" }}
+              activeTextStyle={{
+                fontSize: 15,
+                color: "#fff",
+                fontWeight: "bold"
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  alignContent: "center",
+                  justifyContent: "center",
+                  padding: 10,
+                  backgroundColor: "#d9d9d9"
+                }}
+              >
+                <WebView
+                  automaticallyAdjustContentInsets={false}
+                  source={{
+                    uri: this.state.facebookUrl
+                  }}
+                  javaScriptEnabled={true}
+                  domStorageEnabled={true}
+                  decelerationRate="normal"
+                  startInLoadingState={true}
+                  scalesPageToFit={true}
+                />
+              </View>
+            </Tab>
+          </Tabs>
+          <View>
+            <Footer isOffline={this.state.isOffline} />
+          </View>
+        </Container>
+      );
+    } else {
+      return (
+        <Container style={[styles.root]}>
+          <Loader />
+          <View>
+            <Footer isOffline={this.state.isOffline} />
+          </View>
 
-
-      <Container style={[styles.root]}>
-        <Tabs tabBarUnderlineStyle={{borderColor:'#00ffff'}} style={{ elevation: 3,width:400}} style={styles.tabContent}
-                        onChangeTab={() => {   }}>
-        <Tab heading='Twitter'   tabStyle={{backgroundColor : '#f20505'}} activeTabStyle={{backgroundColor : '#f20505'}}  textStyle={{fontSize:15,color:'#fff'}} activeTextStyle={{fontSize:15,color:'#fff',fontWeight:'bold'}}> 
-                        <View style={{flex:1,alignContent:'center',justifyContent:'center',padding:10,backgroundColor:'#d9d9d9'}}>
-     
-          <WebView
-            style={{flex:1}}
-            originWhitelist={['*']}
-            source={{html: '<a class="twitter-timeline" href="https://twitter.com/TiEPune?ref_src=twsrc%5Etfw">Tweets by TiEPune</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'}}
-            />   
-                        </View>        
-    
-          </Tab>
-        <Tab heading='Facebook' tabStyle={{backgroundColor : '#f20505'}} activeTabStyle={{backgroundColor : '#f20505'}}  textStyle={{fontSize:15,color:'#fff'}} activeTextStyle={{fontSize:15,color:'#fff',fontWeight:'bold'}}>
-              <View style={{flex:1,alignContent:'center',justifyContent:'center',padding:10,backgroundColor:'#d9d9d9'}}>
-              <WebView
-automaticallyAdjustContentInsets={false}
-source={{uri: 'https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftiepune%2F&tabs=timeline&small_header=true&hide_cover=false&show_facepile=false&appId=1279706002103925'}}
-javaScriptEnabled={true}
-domStorageEnabled={true}
-decelerationRate="normal"
-startInLoadingState={true}
-scalesPageToFit={true}
-/>
-              
-            </View>        
-            </Tab>  
-      </Tabs>
-                  <View>
-                  <Footer isOffline ={this.state.isOffline}/>    
-                  </View>
-        </Container>)
-
-
-
-
-
-
-
-
-
-
-   
-//https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fsitrccomp%2F&tabs=timeline&appId=1279706002103925
-
-{/* <View style={{flex:1}}>
-<WebView
-style={{flex:1}}
-originWhitelist={['*']}
-  source={{html: '<a class="twitter-timeline" href="https://twitter.com/TiEPune?ref_src=twsrc%5Etfw">Tweets by TiEPune</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'}}
-/>
-</View> */}
-
-    // <Container style={[styles.root]}>
-    //                 <Tabs style={{ elevation: 3,width:400}} style={styles.tabContent}
-    //                     onChangeTab={() => {
-    //                     //this.fetchSessionList(eventId, userId);
-    //                     }}
-    //                   >
-    //     <Tab  
-    //       heading={
-    //         <TabHeading  style={{backgroundColor : '#fff'}} >
-    //           <Text  style={[styles.textColor]} >Twitter</Text> 
-    //         </TabHeading>
-    //       } style={styles.activeBorder} >        
-                 
-    //         </Tab>
-      //   {/* <Tab
-      //     heading={
-      //       <TabHeading style={{backgroundColor : '#fff'}}>
-      //         <Text  style={[styles.textColor]} >Facebook</Text>
-      //       </TabHeading>
-      //     } style={styles.activeBorder}>
-      //         <View style={{flex:1}}>
-      //         <WebView
-      //         scalesPageToFit={true}
-                            
-      //         style={{flex:1}}
-      //         originWhitelist={['*']}
-      //           source={{uri: '<div style="position: relative;padding-bottom: 1%;padding-top: 1%; height: 100%; width: 100%;  overflow: hidden;"> <iframe src="https://www.facebook.com/tiepune/"  style="position: absolute; top:0;left: 0; width: 100%;height: 100%;" allowfullscreen="true" frameborder="0">  </iframe>  </div>'}}
-      //         />
-              
-      //       </View>        
-      //       </Tab>  
-      // </Tabs>
-      //             <View>
-      //             <Footer isOffline ={this.state.isOffline}/>    
-      //             </View>
-      //   </Container>*/})
-   
-    // return( 
-     
-   
-    // )
-    //  return( 
-   //                source={{html: '<div style="position: relative;padding-bottom: 1%;padding-top: 1%; height: 100%; width: 100%;  overflow: hidden;"> <iframe src="https://www.facebook.com/tiepune/"  style="position: absolute; top:0;left: 0; width: 100%;height: 100%;" allowfullscreen="true" frameborder="0">  </iframe>  </div>'}}
-
-      // <WebView
-      // scalesPageToFit={true}
-      // style={{flex:1,alignContent:'stretch',height:300}}
-      // originWhitelist={['*']}
-      //   source={{html: '<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftiepune%2F&tabs=timeline&width=340&height=400&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true&appId" width="340" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>'}}
-      // />
-    // )
-    // return( 
-    //   <View style={{flex:1}}>
-    //   <WebView
-    //   style={{flex:1,width:'100%',height:'100%'}}
-    //   originWhitelist={['*']}
-    //     source={{html: '<script src="https://platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script>   <script type="IN/FollowCompany" data-id="1337" data-counter="top"></script>'}}
-    //   />
-    //         </View>
-   
-    // )
-    
-   }
+        </Container>
+      );
+    }
+  }
 }
 
 let styles = RkStyleSheet.create(theme => ({
@@ -228,53 +232,49 @@ let styles = RkStyleSheet.create(theme => ({
     backgroundColor: theme.colors.screen.base,
     marginTop: 30
   },
- heading: {
-      paddingBottom: 12.5
-    },
+  heading: {
+    paddingBottom: 12.5
+  },
   column: {
     borderColor: theme.colors.border.base,
     alignItems: "flex-start",
-    paddingLeft:20,
-    paddingRight:20
-
-    },
-    profileImageStyle: {
-      borderColor: theme.colors.border.base,
-      alignItems: "center"  
-      },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 40,
-    borderColor :'black' 
+    paddingLeft: 20,
+    paddingRight: 20
   },
-  text:{
-    alignSelf: 'stretch',
-    fontSize : 17,
-    height:35,
-    borderLeftColor: '#fff',
-    borderTopColor: '#fff',
-    borderRightColor: '#fff',
-    borderBottomColor: '#333',
+  profileImageStyle: {
+    borderColor: theme.colors.border.base,
+    alignItems: "center"
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+    borderColor: "black"
+  },
+  text: {
+    alignSelf: "stretch",
+    fontSize: 17,
+    height: 35,
+    borderLeftColor: "#fff",
+    borderTopColor: "#fff",
+    borderRightColor: "#fff",
+    borderBottomColor: "#333",
 
     borderWidth: 1,
-    paddingLeft:4
-
-    },
-    screen: {
-      backgroundColor: theme.colors.screen.base
-    },
-    tabContent: {
-      backgroundColor: '#FFFFFF',
-    },
-    textColor : {
-      color: '#ed1b24',
-      fontSize:11
-    },
-    activeBorder:{
-      borderColor: '#ed1b24', 
-    }
+    paddingLeft: 4
+  },
+  screen: {
+    backgroundColor: theme.colors.screen.base
+  },
+  tabContent: {
+    backgroundColor: "#FFFFFF"
+  },
+  textColor: {
+    color: "#ed1b24",
+    fontSize: 11
+  },
+  activeBorder: {
+    borderColor: "#ed1b24"
+  }
 }));
-
-
